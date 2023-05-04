a989ffcf6314a18b54d1796c1a0758993bd1f704
mrcsmcl <mrcsmcl@live.com>
2023-05-04 11:51:48 -0300

    Create Cogumelos Por Todos os Lados.c

---
+++ Afinando Sua Lógica de Programação em C/Cogumelos Por Todos os Lados.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int minutos, cogumelosColhidos;
+
+  // Lê a quantidade de minutos disponíveis para colheita
+  scanf("%d", &minutos);
+
+  // TODO: Calcule e imprima a quantidade de cogumelos colhidos.
+
+    cogumelosColhidos = minutos / 3;
+
+    printf("%d", cogumelosColhidos);
+
+  return 0;
+}
